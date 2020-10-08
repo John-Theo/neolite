deefadbe556a345eef1aa23c18e9f87f261c1ebe
Zhuang Xinhao <xinhaozhuang.work@gmail.com>
2020-10-08 15:55:36 +0800

    update tests

--- setup.py
+++ setup.py
@@ -1,20 +1,20 @@
-import setuptools
+from setuptools import find_packages, setup
 
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 
-setuptools.setup(
+setup(
     name="neolite",
-    version="0.7.3",
+    version="0.7.4",
     author="John Theo",
     author_email="xinhaozhuang.work@gmail.com",
     description="Super light weight neo4j python driver with multi-graph support.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/John-Theo/neolite",
-    packages=setuptools.find_packages(),
+    packages=find_packages(exclude=("tests",)),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
--- tests/test_all.py
+++ tests/test_all.py
@@ -38,19 +38,11 @@ def test_auth(auth_graph):
 
 
 def test_no_auth(no_auth_graph):
-    result = no_auth_graph.cypher('''
-        CREATE ( bike:Bike { weight: 10 } ) 
-        CREATE ( frontWheel:Wheel { spokes: 3 } ) 
-        CREATE ( backWheel:Wheel { spokes: 32 } ) 
-        CREATE p1 = (bike)-[:HAS { position: 1 } ]->(frontWheel) 
-        CREATE p2 = (bike)-[:HAS { position: 2 } ]->(backWheel) 
-        RETURN bike, p1, p2
-    ''', stats=True)
-    assert result
-    assert set(result.keys()) == {'nodes', 'relationships'}
-    assert set(result['nodes'][0].keys()) == {'id', 'labels', 'properties'}
-    assert set(result['relationships'][0].keys()) == {'id', 'type', 'properties', 'startNode', 'endNode'}
-
+    with pytest.raises(CypherError) as excinfo:
+        result = no_auth_graph.cypher('''
+            RETURN a
+        ''', stats=True)
+        assert 'Neo.ClientError.Security.Unauthorized' in str(excinfo.value)
 
 def test_wrong_cypher(no_auth_graph):
     with pytest.raises(CypherError) as excinfo:
